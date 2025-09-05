0dc9fd9dbe1884b720155cdfd64455d85a6902a6
Jordyn Geiger <jordyn.geiger1@gmail.com>
2025-09-05 15:54:25 -0400

    add constants, basic game loop

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import pygame
+from constants import (
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_SPAWN_RATE,
+    ASTEROID_MAX_RADIUS,
+)
+
+BLACK = (0, 0, 0)
+
+
 def main():
-    print("Hello from asteroids!")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(BLACK)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
