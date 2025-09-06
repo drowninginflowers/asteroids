034cc1dd0e253b26b721fa204c9abbf6c81d6451
drowninginflowers <jordyn.geiger1@gmail.com>
2025-09-06 02:13:00 -0400

    add player movement, use groups

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
     BACKGROUND_COLOR,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_KINDS,
-    ASTEROID_SPAWN_RATE,
-    ASTEROID_MAX_RADIUS,
+    # ASTEROID_MIN_RADIUS,
+    # ASTEROID_KINDS,
+    # ASTEROID_SPAWN_RATE,
+    # ASTEROID_MAX_RADIUS,
 )
 
 
@@ -15,18 +15,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatables.update(dt)
 
         screen.fill(BACKGROUND_COLOR)
-        player.draw(screen)
+        for sprite in drawables:
+            sprite.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60fps
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_COLOR, PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_COLOR, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 
@@ -19,6 +19,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -26,6 +30,10 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(-dt)
 
     def draw(self, screen):
         pygame.draw.polygon(screen, PLAYER_COLOR, self.triangle(), 2)
